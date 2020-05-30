a6e8e055f93d5ddab51977bcbbbe601e9558008e
Gene Backlin <gbacklin@marizack.com>
2020-05-29 23:55:43 -0500

    Building

---
+++ TestXCFrameworkiOSUI/Frameworks/watchOS/UniKitWatch.xcframework/watchos-armv7k_arm64_32/UniKitWatch.framework/Headers/UniKitWatch.h
@@ -0,0 +1,19 @@
+//
+//  UniKitWatch.h
+//  UniKitWatch
+//
+//  Created by Gene Backlin on 5/29/20.
+//  Copyright © 2020 Gene Backlin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UniKitWatch.
+FOUNDATION_EXPORT double UniKitWatchVersionNumber;
+
+//! Project version string for UniKitWatch.
+FOUNDATION_EXPORT const unsigned char UniKitWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UniKitWatch/PublicHeader.h>
+
+
