779f956952d315ca0784b66816cf35b36da553ee
Gene Backlin <gbacklin@marizack.com>
2020-05-30 00:45:44 -0500

    Working version with using regular framework and Workspace

---
+++ WithWorkspace/TestWatch/TestXCFrameworkiOSUI/TestXCFrameworkWatchUI Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  TestXCFrameworkWatchUI Extension
+//
+//  Created by Gene Backlin on 5/29/20.
+//  Copyright © 2020 Gene Backlin. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ WithWorkspace/TestWatch/UniKit/UniKit/Classes/UniKit.swift
@@ -0,0 +1,16 @@
+//
+//  UniKit.swift
+//  UniKit
+//
+//  Created by Gene Backlin on 7/27/19.
+//  Copyright © 2019 Gene Backlin. All rights reserved.
+//
+
+import Foundation
+
+public class UniKit {
+    
+    public static func hello(_ name: String) -> String {
+        return "Hello \(name)"
+    }
+}
