2b3f32c5da48669c45f0eb34cf5eb5075cc74536
Gene Backlin <gbacklin@marizack.com>
2020-05-31 01:18:26 -0500

    IT works on the watch ! I submitted through TestFlight with all Icons for the Watch App. and the XCFramework loads up and runs on the watch

---
+++ TestXCF/UtilKitWatch.xcframework/watchos-i386-simulator/UtilKitWatch.framework/Headers/UtilKitWatch.h
@@ -0,0 +1,19 @@
+//
+//  UtilKitWatch.h
+//  UtilKitWatch
+//
+//  Created by Gene Backlin on 5/30/20.
+//  Copyright © 2020 Gene Backlin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UtilKitWatch.
+FOUNDATION_EXPORT double UtilKitWatchVersionNumber;
+
+//! Project version string for UtilKitWatch.
+FOUNDATION_EXPORT const unsigned char UtilKitWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UtilKitWatch/PublicHeader.h>
+
+
